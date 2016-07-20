cdc37c72c4abbe63dfb14d36ead89c7706fb77b9
Gail Muldoon <gailrmuldoon+github@gmail.com>
2016-07-20 13:54:44 -0400

    Streamlined filenaming convention for reading data more straightforwardly

--- Data/Data_tests.m
+++ Data/Data_tests.m
@@ -1,23 +1,26 @@
 %{
-Reading in bathymetric survey data collected by the BathyDuck Product at
+Sandbox for reading in data collected by the BathyDuck Product at
 ERDC. Includes data at the boundary condition pt and from the beach-mounted
 video.
 
 Data comes directly from the url
 http://chlthredds.erdc.dren.mil/thredds/catalog/frf/projects/bathyduck/data/catalog.html
 
+The useful parts of this script have been converted to functions for use in
+modeling. 
 %}
 
 %% Boundary Condition Data %% 
 % Load in Boundary Condition data from bathyduck/data 
 % FRF-ocean_waves_awac04_201510
+filename='http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/data/FRF-ocean_waves_awac04_201510.nc';
 
-bc.time=ncread('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/data/FRF-ocean_waves_awac04_201510.nc?waveHs[0:1:644],wavePeakFrequency[0:1:644],time[0:1:644],depth[0:1:0],lat,lon','time');
-bc.waveHs=ncread('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/data/FRF-ocean_waves_awac04_201510.nc?waveHs[0:1:644],wavePeakFrequency[0:1:644],time[0:1:644],depth[0:1:0],lat,lon','waveHs');
-bc.wavePeakFrequency=ncread('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/data/FRF-ocean_waves_awac04_201510.nc?waveHs[0:1:644],wavePeakFrequency[0:1:644],time[0:1:644],depth[0:1:0],lat,lon','wavePeakFrequency');
-bc.depth=ncread('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/data/FRF-ocean_waves_awac04_201510.nc?waveHs[0:1:644],wavePeakFrequency[0:1:644],time[0:1:644],depth[0:1:0],lat,lon','depth');
-bc.lat=ncread('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/data/FRF-ocean_waves_awac04_201510.nc?waveHs[0:1:644],wavePeakFrequency[0:1:644],time[0:1:644],depth[0:1:0],lat,lon','lat');
-bc.lon=ncread('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/data/FRF-ocean_waves_awac04_201510.nc?waveHs[0:1:644],wavePeakFrequency[0:1:644],time[0:1:644],depth[0:1:0],lat,lon','lon');
+bc.time=ncread(filename,'time');
+bc.waveHs=ncread(filename,'waveHs');
+bc.wavePeakFrequency=ncread(filename,'wavePeakFrequency');
+bc.depth=ncread(filename,'depth');
+bc.lat=ncread(filename,'lat');
+bc.lon=ncread(filename,'lon');
 
 %% Bathy Data %%
 % Load in bathymetric data from bathyduck/data
@@ -27,7 +30,6 @@ bc.lon=ncread('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyd
 
 %The following are for identifying the filename and individual data points
 point_input = [04,11,12,13,14,22,23,24,83]';
-max_ind_input = [6931,5497,5493,3047,3908,5452,5450,4014,5165];
 
 %Create an empty structure
 bathydata = struct;
@@ -35,7 +37,7 @@ bathydata = struct;
 %Iterate over files (each containing 1 spatial data pt), read select fields,
 %save to data structure with one cell array for each file's data
 for i = 1:length(point_input)
-    filename = sprintf('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/data/BathyDuck-ocean_bathy_p%02d_201510.nc?time[0:1:%d],xloc,yloc,seafloorLocation[0:1:%d]',point_input(i),max_ind_input(i),max_ind_input(i));
+    filename = sprintf('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/data/BathyDuck-ocean_bathy_p%02d_201510.nc',point_input(i));
     fieldname = sprintf('p%02d',point_input(i));
     
     time = ncread(filename,'time');
@@ -54,7 +56,6 @@ end
 
 %Params for getting filenames, identifying pts
 point_input = [11,12,13,14,21,22,23,24,83,84];
-max_ind_input = [474,451,474,474,450,450,473,451,426,357];
 
 %Intitiate empty data structure
 wavedata = struct; 
@@ -62,7 +63,7 @@ wavedata = struct;
 %Iterate over files (each containing 1 spatial data pt), read select fields,
 %save to data structure with one cell array for each file's data
 for i = 1:length(point_input)
-   filename = sprintf('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/data/BathyDuck-ocean_waves_p%02d_201510.nc?time[0:1:%d],zloc[0:1:%d],waveHs[0:1:%d],waveHsArray[0:1:%d][0:1:5],xloc[0:1:%d],yloc[0:1:%d]',point_input(i),max_ind_input(i),max_ind_input(i),max_ind_input(i),max_ind_input(i),max_ind_input(i),max_ind_input(i));
+   filename = sprintf('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/data/BathyDuck-ocean_waves_p%02d_201510.nc',point_input(i));
    fieldname = sprintf('p%02d',point_input(i));
    
    time = ncread(filename,'time');
@@ -113,15 +114,13 @@ legend('Bathy Points', 'Wave Points','location','northwest');
 date = [20151030,20151023,20151021,20151019,20151014,20151008,20151001];
 ptnum = [8,7,6,5,3,2,1];
 version = [20151030,20151028,20160104,20151030,20151019,20151009,20151001];
-max_ind_lat = [32,41,58,41,57,41,11];
-max_ind_lon = [59,55,71,55,192,54,54];
 instrument = ['CRAB';'CRAB';'LARC';'CRAB';'LARC';'CRAB';'CRAB'];
 
 %Iterate over files (each containing 1 spatial data pt), read select fields,
 %save to data structure with one cell array for each file's data
 for i = 1:length(date)
    
-   filename = sprintf('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/survey/gridded/FRF_BD_%d_%04d_NAVD88_%s_GPS_UTC_v%d_grid.nc?time[0:1:0],lat[0:1:%d],lon[0:1:%d],FRF_Yshore[0:1:%d],FRF_Xshore[0:1:%d],elevation[0:1:0][0:1:%d][0:1:%d]',date(i),ptnum(i),instrument(i,:),version(i),max_ind_lat(i),max_ind_lon(i),max_ind_lat(i),max_ind_lon(i),max_ind_lat(i),max_ind_lon(i));
+   filename = sprintf('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/survey/gridded/FRF_BD_%d_%04d_NAVD88_%s_GPS_UTC_v%d_grid.nc',date(i),ptnum(i),instrument(i,:),version(i));
    fieldname = sprintf('gd%04d',ptnum(i));
     
    time = ncread(filename,'time');
@@ -154,13 +153,12 @@ end
 
 %params to id urls with data files
 date = {'201602','201601','201512','201511','201510_08','201510'};
-ntime=[600,701,734,734,23,953];
 
 %Iterate over data files, read select fields,save to data structure with 
 %one cell array for each file's data
 for i=1:length(date)
     datei = date{i};
-    filename = sprintf('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/data/BathyDuck-ocean_bathy_argus_%s.nc?time[0:1:%d],xm[0:1:72],ym[0:1:80],depth[0:1:0][0:1:0][0:1:0],depthKF[0:1:0][0:1:0][0:1:0],depthKFError[0:1:0][0:1:0][0:1:0],fB[0:1:%d][0:1:80][0:1:72][0:1:3],k[0:1:%d][0:1:80][0:1:72][0:1:3]',datei,ntime(i),ntime(i),ntime(i));
+    filename = sprintf('http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/data/BathyDuck-ocean_bathy_argus_%s.nc',datei);
     fieldname= sprintf('d%s',datei);
     
     time = ncread(filename,'time');
