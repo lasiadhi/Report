708656bae7cc3e988a3c3b774dd5ec617d3bf2cf
Gail Muldoon <gailrmuldoon+github@gmail.com>
2016-07-20 15:42:55 -0400

    Function to look at header info in BC data file to get field names and dimensions

--- Data/dispBC.m
+++ Data/dispBC.m
@@ -4,6 +4,9 @@ This function displays header information for the fields contained in the
 bathyduck 1D boundary condition data file. This is especially useful for
 finding fields of interest to load using the getBC function. It also gives
 info about the size of the fields when subselecting a time slice to load.
+
+USAGE:
+> dispBC()
 %}
 
 url='http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/data/FRF-ocean_waves_awac04_201510.nc';
