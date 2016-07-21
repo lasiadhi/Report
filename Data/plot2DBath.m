1a80b0281eccf85fdb4ed955aa89b00328179342
Gail Muldoon <gailrmuldoon+github@gmail.com>
2016-07-21 11:56:34 -0400

    Minor update to figure, added usage statement

--- Data/plot2DBath.m
+++ Data/plot2DBath.m
@@ -2,6 +2,9 @@ function plot2DBath
 %{
 Plot 2D Bathymetry data in lat/lon coordinates. For either one or many
 dates when we have data. Current setup is for only plotting Oct 1.
+
+USAGE:
+plot2DBath
 %}
     %get bathymetry data
     [bathdata,date]=get2DtrueBath;
@@ -15,7 +18,7 @@ dates when we have data. Current setup is for only plotting Oct 1.
         surf(bathdata.(fieldname){2},bathdata.(fieldname){3},bathdata.(fieldname){6})
         xlabel('latitude (\circ)')
         ylabel('longitude (\circ)')
-        zlabel('depth (m)')
+        zlabel('elevation (m)')
         datestr=num2str(date(i));
         titstr = sprintf('True bathymetry for %s October 2015',datestr(end-1:end));
         title(titstr)
