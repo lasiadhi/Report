e1b116eee9a2741f18a920e458183bc825bed488
Clint Seinen <clint.seinen@gmail.com>
2016-07-21 19:21:54 -0700

    Added 1D bathymetry plot.

---
+++ Data/plot1DtrueBathy.m
@@ -0,0 +1,26 @@
+function plot1DtrueBathy
+% This function plots the 1D true bathymetry
+    
+    % call in elevations and populate water levels
+    filename = 'http://chlthredds.erdc.dren.mil/thredds/dodsC/frf/projects/bathyduck/survey/transect/FRF_BD_20151001_0001_NAVD88_CRAB_GPS_UTC_v20151001.nc';
+    profileNumber = ncread(filename, 'profileNumber');
+    ii = find(profileNumber == 951);
+    z = ncread(filename, 'elevation',min(ii),length(ii));
+    z = fliplr(z);
+    for i = 1:length(ii)
+        water_level(i) = 0.156;
+    end
+    
+    % call h and x data
+    [h,x] = get_hOct1;
+    
+    % discretize h and x
+    [hgrid,xgrid] = interp_h(h,x,10);
+    
+    figure
+    hold on
+    subplot(1,2,1)
+    plot(x,z);
+    plot(x,water_level,'k-.','Lineweight',2);
+    
+end
