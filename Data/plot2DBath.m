0ffdbf6272c99997fd09b6a2f6d4157d0c0a42cc
Gail Muldoon <gailrmuldoon+github@gmail.com>
2016-07-21 20:02:18 -0400

    Added 1D profile line to 2D bathymetry plot

--- Data/plot2DBath.m
+++ Data/plot2DBath.m
@@ -24,4 +24,18 @@ plot2DBath
         titstr = sprintf('True bathymetry for %s October 2015',datestr(end-1:end));
         title(titstr)
     end
+    
+    
+    % show 1D profile on 2D surface
+    % get indices for FRF_Yshore = 950m 
+    transect_yind = find(bathdata.(fieldname){5}==951.203,1);
+    % show lat/lon corresponding to indices where FRF_Yshore = 950
+    bathprofile_lat = bathdata.(fieldname){2}(transect_yind);
+    %make it a vector of the one value for plotting
+    bathprofile_lat_vector = ones(55,1)*bathprofile_lat;
+    % draw line with z=elevation at indices where FRF_Yshore = 950
+    p=plot3(bathprofile_lat_vector,bathdata.(fieldname){3},bathdata.(fieldname){6}(:,transect_yind),'LineWidth',8,'Color','r','Linestyle','-');
+    legend(p,'Transect in 1D model','Location','northwest')
+    
+    
 end
