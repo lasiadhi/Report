7835aff9b0fa86573927360aa5c42725d5c28748
Wenbin Dong <wdong05@gmail.com>
2016-07-23 15:17:14 -0400

    new initial guess

--- Code/Inverse_problem/Least_square/fminsearch_least_squares.m
+++ Code/Inverse_problem/Least_square/fminsearch_least_squares.m
@@ -5,7 +5,7 @@ N = 47; %116;  %47
 dx = 25; %10;  %25
 [h,x] = get_hOct1;
 [hgrid,xq] = interp_h(h,x,dx);
-h_guess = initialize_h_guess(hgrid,dx);
+h_guess = initialize_h_guess_pointwise(hgrid, xq, dx);
 
 load('k_1percNoisedata_N47.mat','k'); 
 
@@ -14,11 +14,11 @@ load('k_1percNoisedata_N47.mat','k');
 % fmincon: Find minimum of constrained nonlinear multivariable function
 %options = optimoptions('Display','off');
 options  = optimset('Display', 'iter', 'TolFun', 1e-4, 'MaxIter', 20);          %,'MaxIter',10   %,'TolFun',1e-3
-h_hat3   = fminsearch(@objective_2norm, repmat(5,[N,1]), options);              %, [], [], [], [], zeros(N,1), repmat(12, [N,1]), []
+h_hat3   = fminsearch(@objective_2norm, h_guess, options);              %, [], [], [], [], zeros(N,1), repmat(12, [N,1]), []
 
 
 figure(3)
-stem(hgrid, 'b');
+stem(hgrid, '*b');
 hold on
 stem(h_hat3, 'r');
 title('fmincon method', 'fontSize',14)
