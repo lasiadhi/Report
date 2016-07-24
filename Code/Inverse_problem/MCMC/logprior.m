4c4f1309a2ff2e506945aff8afb73fe393301e5f
Gail Muldoon <gailrmuldoon+github@gmail.com>
2016-07-24 14:20:26 -0400

    Latest MCMC

--- Code/Inverse_problem/MCMC/logprior.m
+++ Code/Inverse_problem/MCMC/logprior.m
@@ -1,16 +1,22 @@
-function [prior,depthmean,depthstd] = logprior(h,bath)
+function [prior,fudgestd] = logprior(h,bath)
 %{
 Need a prior on what we want: depth, h
 Get this from previous observations. Use this to build a prior distribution
 on h
 %}
-
+xvec=(0:25:1150);
 depth = bath.depth;
 
 %Assume depth distribution is normal and find mean/std at each x
 depthmean = nanmean(depth,2); %mean over time dimension
 depthstd = std(depth,0,2);
 
-prior = log(normpdf(h,depthmean,depthstd));
+depthmeanInterp = interp1(depthmean,xvec);
+fudgestd = 0.5;
+
+
+
+%prior = log(normpdf(h,depthmeanInterp',fudgestd));
+prior = log(normpdf(h,0,fudgestd));
 
 end
