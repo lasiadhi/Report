65d6dd29bf36209596e1a0834402f6e6e500ddca
Olaniyi Iyiola <osiyiola@uwm.edu>
2016-07-25 10:31:39 -0500

    Changes to real data in NLTR_least_Square

--- Code/Inverse_problem/Least_square/NLTR_least_Square.m
+++ Code/Inverse_problem/Least_square/NLTR_least_Square.m
@@ -1,5 +1,6 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % lsqnonlin: trust-region-reflective method
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clc
 N = 116;
 dx = 10;
@@ -7,18 +8,18 @@ dx = 10;
 [hgrid,xq] = interp_h(h,x,dx);
 %h_guess = initialize_h_guess(hgrid,dx);
 h_guess = initialize_h_guess_pointwise(hgrid, xq, dx);
-%h_guess = h_guess';
 
-%int_h = hgrid + 0.1 * randn(N,1);
+load('Real_data_2015-10-09_T215900_10m.mat','k_data','x_data');   % load real k_data
 
-
-load('k_2_5percNoisedata.mat','k'); 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 options = optimoptions('lsqnonlin','Display','iter');
-[h_hat,resnorm2] = lsqnonlin(@objective, h_guess+1e-5, zeros(N,1),repmat(12,[N,1]),options);
+[h_hat,resnorm2] = lsqnonlin(@objective_real, h_guess+1e-5, zeros(N,1),repmat(12,[N,1]),options);
 resnorm2;
-%resnorm2_1 = norm(ht - h_hat2)
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Plots for nonlinear least square results
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(2)
 plot(xq,hgrid, '-*b');
 hold on
@@ -30,4 +31,4 @@ set(gca,'ydir','reverse')
 set(gca,'xdir','reverse')
 xlabel('Distance from the coastline','FontSize',14);
 ylabel('depth (h) m','FontSize',14);
-legend({'True h', 'Recovered h','Initial guess'},'FontSize',14);
+legend({'True Bathymetry', 'Recovered Bathymetry','Initial guess'},'FontSize',14);
