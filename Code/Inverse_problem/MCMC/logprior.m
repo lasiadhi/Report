af687ed34f534041d5501536b30b08b182b4fd90
Gail Muldoon <gailrmuldoon+github@gmail.com>
2016-07-25 10:27:41 -0400

    Moving forward problem figs and updating prior

--- Code/Inverse_problem/MCMC/logprior.m
+++ Code/Inverse_problem/MCMC/logprior.m
@@ -1,4 +1,4 @@
-function [prior,fudgestd] = logprior(h,bath)
+function [prior] = logprior(h,bath)
 %{
 Need a prior on what we want: depth, h
 Get this from previous observations. Use this to build a prior distribution
@@ -7,18 +7,20 @@ on h
 
 
 %Assume depth distribution is normal and find mean/std at each x
-depth = bath.depth;
-depthmean = nanmean(depth,2); %mean over time dimension
-depthstd = std(depth,0,2);
+% depth = bath.depth;
+% depthmean = nanmean(depth,2); %mean over time dimension
+% depthstd = std(depth,0,2);
 
-xvec=(1:10:1150);
-depthmeanInterp = interp1(depthmean,xvec);
-fudgestd = 0.25;
+% xvec=(1:10:1150);
+% depthmeanInterp = interp1(depthmean,xvec);
+% fudgestd = 0.25;
 
+mu=0;
+sigma=1;
 
-
-prior = log(normpdf(h,flip(depthmean),depthstd));
-prior = log(normpdf(h,flip(depthmean),fudgestd));
+prior = sum(log(normpdf(h,mu,sigma)));
+%prior = log(normpdf(h,flip(depthmean),depthstd));
+%prior = log(normpdf(h,flip(depthmean),fudgestd));
 %prior = log(normpdf(h,depthmeanInterp',depthstd));
 %prior = log(normpdf(h,0,fudgestd));
 
