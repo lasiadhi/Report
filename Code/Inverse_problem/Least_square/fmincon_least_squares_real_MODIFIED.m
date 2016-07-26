ebcf5545192e4a34650dc287d267f8ffe0289cb1
ladhikari <ladhikari@ucmerced.edu>
2016-07-25 23:38:31 -0400

    added all the modifed and new files

--- Code/Inverse_problem/Least_square/fmincon_least_squares_real_MODIFIED.m
+++ Code/Inverse_problem/Least_square/fmincon_least_squares_real_MODIFIED.m
@@ -12,47 +12,62 @@ N  	= size(x_data,1);
 %h_guess    = initialize_h_guess_pointwise_16(hgrid, xq, dx);
 h_guess    	= initialize_h_guess_pointwise(hgrid, xq, dx);
 
+% Pick NaN locations from k_data
+nanlocations = ~isnan(k_data);
+h_guess_thresh = h_guess(nanlocations); % Chopped h_guess
+N_chopped = length(h_guess_thresh);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % fmincon: Find minimum of constrained nonlinear multivariable function
 %options = optimoptions('Display','off');
-options = optimset('Display','iter', 'MaxFunEvals', 20000);
+options = optimset('Display','iter', 'MaxFunEvals', 15000);
 
-h_hat3    = fmincon(@objective_2norm_real, h_guess, [],[],[],[], zeros(N,1), repmat(11,[N,1]),[], options);
+h_hat3    = fmincon(@objective_2norm_real_MODIFED, h_guess_thresh, [],[],[],[], zeros(N_chopped,1), repmat(11,[N_chopped,1]),[], options);
 
 
 % Remove points where there are no measurements for K and replace
 % Linear interpolation
-% We know h = 0 at h(1)
-num = 2;
-h_approx_subset(1) = 0;
-x_subset(1) = 1150;
 
-for i = 2:length(h_guess)
-	if ~isnan(k_data(i))
-		h_approx_subset(num) = h_hat3(i);
-		x_subset(num) = xq(i);
-		num = num + 1;
-	else
-	end
-end
-h_approx_subset(num) = 11;
-x_subset(num) = 0;
+% num = 2;
+% h_approx_subset(1) = 0;
+% x_subset(1)        = 1150;
 
-h_approx = interp1(x_subset,h_approx_subset,xq,'linear');
+
+%nanlocations(nanlocations>=0) = h_hat3;
+h_processed = zeros(N,1);
+h_processed(nanlocations) = h_hat3;
+h_processed(1) = 11;
+nanlocations = [1; nanlocations(2:end-1); 1];
+%h_processed(1)  = 0;  % We know h = 0 at h(1)
+
+
+% for i = 2:length(h_guess)
+% 	if ~isnan(k_data(i))
+% 		h_approx_subset(num) = h_hat3(i);
+% 		x_subset(num) = xq(i);
+% 		num = num + 1;
+% 	else
+% 	end
+% end
+
+%x_subset(num) = 0;
+
+
+h_intep = interp1(x_data(logical(nanlocations)),h_processed(logical(nanlocations)),xq,'linear');
 
 figure(3)
 subplot(2,1,1)
 plot(xq,hgrid, '-b');
 hold on
 % plot(xq,h_hat3, '-^r');
-plot(xq,h_approx, '-r');
-title('fmincon Method with Real Data', 'fontSize',14);
+plot(xq,h_intep, '-r');
+title('fmincon Method with Real Data - Modified', 'fontSize',14);
 xlim([0,1150])
 xlabel('x Position (m)','FontSize',14);
 ylabel('Depth (m)','FontSize',14);
 set(gca,'ydir','reverse')
 set(gca,'xdir','reverse')
 hold on
+%xq_thres = xq(logical(nanlocations(2:end)));
 plot(xq,h_guess,'g')
 legend({'True h','Processed Recovered h','Initial guess'},'FontSize',14);
 
---
+++ Code/Inverse_problem/Least_square/objective_2norm_real_MODIFED.m
@@ -0,0 +1,28 @@
+function f = objective_2norm_real_MODIFED(h)
+
+	load('Real_data_2015-10-09_T215900_10m.mat','k_data','x_data');   % load real k_data
+	dx = x_data(2) - x_data(1); % resolution defined by data grid
+
+	[k_appro_subset, H] = forward_real(h,dx);
+
+% 	num = 1;
+% 	for i = 1:size(k_data)
+% 		if ~isnan(k_data(i))
+% 			k_appro_subset(num) = k_appro(i);
+% 			k_data_subset(num)	= k_data(i);
+% 			h_subset(num) 		= h(i);
+% 			num 				= num + 1;
+% 		else	
+% 		end
+%     end
+    
+    
+    k_data_subset = k_data(~isnan(k_data));
+    len    = length(h);
+    lambda = zeros(len,1);
+    lambda(1:ceil(len/2))     = 1e-8;
+    lambda(ceil(len/2)+1:end) = 1e-8;
+
+	f =  norm(k_appro_subset - k_data_subset)^2 +  norm(sqrt(lambda) .* h)^2;
+
+end
